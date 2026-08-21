2fbf00fe548b23e4e0a90eb64b81c7825c2487b5
Tomas <inostrozatomas100@gmail.com>
2026-08-21 12:22:54 -0400

    XD

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+import "./global.css";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+
+  );
+}
